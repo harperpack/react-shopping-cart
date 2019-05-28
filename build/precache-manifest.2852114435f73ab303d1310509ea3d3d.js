e2b0a535d5b49d7307d66d55223a7e560c141ebb
Harper <charlespack2019@u.northwestern.edu>
2019-05-28 12:17:43 -0500

    Failign to use firebase correctly

---
+++ build/precache-manifest.2852114435f73ab303d1310509ea3d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd23f8dacda6a4416608b78390c4d79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12c2fd2c6a2d82cd6915",
+    "url": "/static/css/main.1c83be7c.chunk.css"
+  },
+  {
+    "revision": "275c4385944c5c06f543",
+    "url": "/static/js/2.792acef6.chunk.js"
+  },
+  {
+    "revision": "12c2fd2c6a2d82cd6915",
+    "url": "/static/js/main.0ad2773f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
